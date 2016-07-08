107cbcdd0455038e2792c7bb6c258d1baf4f408c
smok998 <drogonlukasz@wp.pl>
2016-07-08 12:07:38 +0200

    poprawa tabeli wsp

--- database/migrations/2016_07_08_075904_wsp_tabela.php
+++ database/migrations/2016_07_08_075904_wsp_tabela.php
@@ -12,33 +12,33 @@ class WspTabela extends Migration
      */
     public function up()
     {
-        Schema::create('"wsp"', function (Blueprint $table) {
+        Schema::create('wsp', function (Blueprint $table) {
             $table->increments('id');
-            $table->tinyInteger('dwop_id');
-            $table->tinyInteger('kategoria_id');
-            $table->tinyInteger('przeznaczenie_id');
-            $table->tinyInteger('rodzaj_id');
-            $table->mediumInteger('pojazdy_id');
-            $table->mediumInteger('sprzet_id');
-            $table->integer('zdarzenia_id');
-            $table->mediumInteger('strazacy_id');
-            $table->integer('stanzmianowy_id');
-            $table->char('jednostka');
-            $table->char('typ'); //z lity
-            $table->char('ksrg'); //z lity TAK lub NIE
-            $table->char('nazwa_skrot');
-            $table->char('miejscowosc');
+            $table->Integer('dwop_id');
+            $table->Integer('kategoria_id');
+            $table->Integer('przeznaczenie_id');
+            $table->Integer('rodzaj_id');
+            $table->Integer('pojazdy_id');
+            $table->Integer('sprzet_id');
+            $table->Integer('zdarzenia_id');
+            $table->Integer('strazacy_id');
+            $table->Integer('stanzmianowy_id');
+            $table->char('jednostka,255');
+            $table->enum('typ', ['DWOP','SSP','ZRG']);
+            $table->enum('ksrg', ['TAK','NIE']);
+            $table->char('nazwa_skrot,255');
+            $table->char('miejscowosc,255');
             $table->tinyInteger('etat_pracownicy');
             $table->tinyInteger('etat_strazacy');
             $table->tinyInteger('etat_zolnierze');
-            $table->integer('tel_alarmowy');
-            $table->integer('tel_sluzbowy');
-            $table->integer('tel_komorka_komendant');
-            $table->integer('tel_sluzbowy_komendant');
-            $table->integer('tel_oficer_dyzurny_jednostki');
-            $table->integer('tel_fax_dowodca');
-            $table->integer('tel_dowodca');
-            $table->char('komendant_wsp');
+            $table->string('tel_alarmowy,15');
+            $table->string('tel_sluzbowy,15');
+            $table->string('tel_komorka_komendant,15');
+            $table->string('tel_sluzbowy_komendant,15');
+            $table->string('tel_oficer_dyzurny_jednostki,15');
+            $table->string('tel_fax_dowodca,15');
+            $table->string('tel_dowodca,15');
+            $table->string('komendant_wsp,100');
             $table->timestamps();
         });
     }
@@ -50,6 +50,6 @@ class WspTabela extends Migration
      */
     public function down()
     {
-        Schema::drop('"wsp"');
+        Schema::drop('wsp');
     }
 }
